0ab746c74f38fe27656f4bbb1f17aa899c77d89c
Emad <m.e89571828@gmail.com>
2023-12-18 18:00:05 +0530

    env file edit

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
-import Window from '../components/Window'   
+import Window from '../components/Window'
 import Editor from '../components/Editor'
 import '../css/note.css'
 import { useState, useEffect } from 'react'
@@ -8,22 +8,23 @@ import { useState, useEffect } from 'react'
 
 export default function Note() {
     const [notes, setNotes] = useState([]);
-  
+
     const [windows, setWindows] = useState([])
     const [isTabOpen, setIsTabOpen] = useState(false)
 
-    function openWindow(Title, Id, setIsOpen){
-        setWindows(prevWindow => [...prevWindow, {title: Title, id: Id}])
+    function openWindow(Title, Id, setIsOpen) {
+        setWindows(prevWindow => [...prevWindow, { title: Title, id: Id }])
         setIsOpen((prevOpen) => !prevOpen)
     }
 
     useEffect(() => {
-      
+
+
     })
 
     useEffect(() => {
         const token = localStorage.getItem("jwtToken")
-        if (!token){
+        if (!token) {
             window.location.href = "/login"
         }
     }, [])
@@ -31,41 +32,41 @@ export default function Note() {
 
     const getAllNotes = async () => {
         try {
-          const res = await fetch("http://localhost:8000/notes", {
-            method: "GET",
-            headers: {
-              authorization: localStorage.getItem("jwtToken"),
-            },
-          });
-    
-          if (res.status === 200) {
-            const data = await res.json();
-            setNotes(data);
-          }
+            const res = await fetch("http://localhost:8000/notes", {
+                method: "GET",
+                headers: {
+                    authorization: localStorage.getItem("jwtToken"),
+                },
+            });
+
+            if (res.status === 200) {
+                const data = await res.json();
+                setNotes(data);
+            }
         } catch (error) {
-          console.log(error.message);
+            console.log(error.message);
         }
-      };
-    
-      useEffect(() => {
+    };
+
+    useEffect(() => {
         getAllNotes();
-      }, []);
-    
+    }, []);
+
     return (
-      <main>
-        <SideBar notes = {notes} openWindow = {openWindow}/>
-        <div className='rigth-section'>
-            <div className='window-container'>
-                {windows.map((window, index) => {
-                    return  <Window  key = {index} title = {window.title}/>
-                })}
+        <main>
+            <SideBar notes={notes} openWindow={openWindow} />
+            <div className='rigth-section'>
+                <div className='window-container'>
+                    {windows.map((window, index) => {
+                        return <Window key={index} title={window.title} />
+                    })}
+                </div>
+                <div className='Writes-and-edit-notes-contianer'>
+                    {isTabOpen ? <Editor /> : <EditorBackground />}
+
+                </div>
             </div>
-            <div className='Writes-and-edit-notes-contianer'>
-                {isTabOpen ? <Editor/> : <EditorBackground/>}
-           
-            </div>
-        </div>
-      </main>
+        </main>
     )
 }
 
@@ -76,9 +77,6 @@ function EditorBackground() {
             <div className='default-page-with-no-editor'>
                 <h1>Write your thoughts</h1>
             </div>
-        
-        
-        
         </>
 
 
--- src/server/server.js
+++ src/server/server.js
@@ -14,26 +14,26 @@ app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.post('/signup',  (req, res) => {
-    const {email , password, confirmPassword} = req.body;
-    console.log(email , password)
+app.post('/signup', (req, res) => {
+    const { email, password, confirmPassword } = req.body;
+    console.log(email, password)
 
-    if (password !== confirmPassword){
-        return res.json({message: "Incorrect! password"})
-        
+    if (password !== confirmPassword) {
+        return res.json({ message: "Incorrect! password" })
+
     }
 
     const sql = 'SELECT * FROM users WHERE user_email = ?'
-    connection.query(sql, [email],  async (err, results) => {
-        if (err){
+    connection.query(sql, [email], async (err, results) => {
+        if (err) {
             console.log("Failed query: ", err.message);
         }
-        else{
-            if (results.length != 0){
+        else {
+            if (results.length != 0) {
                 // It means that the email alredy exist
-                return res.json({message: "Email already exist"})
+                return res.json({ message: "Email already exist" })
             }
-            else{
+            else {
                 // Insert the new user into database
 
                 try {
@@ -42,63 +42,63 @@ app.post('/signup',  (req, res) => {
                     const newUser = [email, hashPassword]
                     const sql2 = 'INSERT INTO users(user_email, user_password) VALUES(?, ?)'
                     connection.query(sql2, newUser, (err, results) => {
-                        if (err){
+                        if (err) {
                             console.log("query failed: ", err.message);
                         }
-                        else{
-                            return res.status(201).json({message: "User created successfully", hashPassword: hashPassword})
+                        else {
+                            return res.status(201).json({ message: "User created successfully", hashPassword: hashPassword })
                         }
                     })
-                }catch(error){
-                   return  res.status(400).send({message: error.message});
+                } catch (error) {
+                    return res.status(400).send({ message: error.message });
                 }
-              
+
             }
         }
     })
 })
 
 
-app.post('/login', async (req, res) =>{
+app.post('/login', async (req, res) => {
 
-    const {email, password} = req.body
+    const { email, password } = req.body
     try {
 
-        const  sql = 'SELECT * FROM users WHERE user_email = ?'
+        const sql = 'SELECT * FROM users WHERE user_email = ?'
         connection.query(sql, [email], async (err, results) => {
-            if (err){
-                console.log("Failed query: " , err.message);
+            if (err) {
+                console.log("Failed query: ", err.message);
             }
             else {
                 // It can be possible that user enter a wrong password so wen need  to chekc here
-                if(results.length === 0){
+                if (results.length === 0) {
                     // means that user with the email does not exist
-                    return res.send({message: "Email not found"})
+                    return res.send({ message: "Email not found" })
                 }
 
                 const userPassword = results[0].user_password
-    
-                if (await bcrypt.compare(password, userPassword)){
+
+                if (await bcrypt.compare(password, userPassword)) {
                     // lets create a token so that we can identify the user in future request
                     const userId = { userId: results[0].id }
                     const token = jwt.sign(userId, process.env.JWT_SECRET)
-    
-                    return res.status(201).send({jwtToken: token})
+
+                    return res.status(201).send({ jwtToken: token })
 
                 }
                 else {
-                    return res.status(400).send({message : "Incorrect!! password"})
-                   
+                    return res.status(400).send({ message: "Incorrect!! password" })
+
                 }
             }
         })
 
     }
-    catch(error){
-        return res.status(400).send({message : error.message})
+    catch (error) {
+        return res.status(400).send({ message: error.message })
     }
 
- 
+
 })
 
 app.post('/logout', auth, (req, res) => {
@@ -106,56 +106,56 @@ app.post('/logout', auth, (req, res) => {
     // put the token in invalidate token so that in future any one access it will not be able to access the private content
     const sql = "INSERT INTO expireTokens(invalidToken) VALUE(?)"
     connection.query(sql, [token], (err, results) => {
-        if (err){
+        if (err) {
             console.log("Failed query : ", err.message)
         }
         else {
-            return res.status(200).send({message: "LogOut successfully"})
+            return res.status(200).send({ message: "LogOut successfully" })
 
         }
     })
-    
+
 })
 
-function auth(req, res, next){
+function auth(req, res, next) {
     const authheader = req.headers["authorization"]
 
-    if (!authheader){
-        return res.status(500).send({message : "Missing auth header"})
+    if (!authheader) {
+        return res.status(500).send({ message: "Missing auth header" })
     }
     // now decode the authorization header
     const decoded = jwt.verify(authheader, process.env.JWT_SECRET)
 
-    if(decoded && decoded.userId){
+    if (decoded && decoded.userId) {
         req.userId = decoded.userId;
         next();
     }
     else {
-        return res.status(500).send({message: "Incorrect!! token"})
+        return res.status(500).send({ message: "Incorrect!! token" })
     }
 }
 
 
 
-app.get('/notes', auth,  (req, res) => {
-    const  userId = req.userId
+app.get('/notes', auth, (req, res) => {
+    const userId = req.userId
     const currentToken = req.headers["authorization"]
     // check the whether currentToken lies in table of invalidate token or not 
     const sql = 'SELECT *FROM expireTokens  WHERE invalidToken = ? '
     connection.query(sql, [currentToken], (err, results) => {
-        if (err){
+        if (err) {
             console.log('Query Failed: ', err.message)
             return res.status(500)
         }
         else {
             // if results length === 0 it means all is ok
-            if (results.length === 0){
+            if (results.length === 0) {
                 // write a new query to fetch all the notes form notes table 
                 const sql1 = 'SELECT *FROM notes WHERE userId = ?'
                 connection.query(sql1, [userId], (err, results) => {
-                    if (err){
+                    if (err) {
                         console.log('Query Failed: ', err.message)
-                        return res.status(500)                        
+                        return res.status(500)
                     }
                     else {
                         return res.status(200).send(results)
@@ -168,7 +168,7 @@ app.get('/notes', auth,  (req, res) => {
 
             }
         }
-    })   
+    })
 })
 
 
@@ -177,12 +177,12 @@ app.post('/postNotes', auth, (req, res) => {
     const userId = req.userId
     const sql = 'INSERT INTO notes(title, userId) VALUES(?, ?)'
     connection.query(sql, [noteTitle, userId], (err, results) => {
-        if (err){
+        if (err) {
             console.log("Query failed: ", err.message)
-            return res.status(500).send({message: "Query failed"})
+            return res.status(500).send({ message: "Query failed" })
         }
         else {
-            return res.status(200).send({message: "notes created successfully"})
+            return res.status(200).send({ message: "notes created successfully" })
         }
     })
 })
@@ -192,21 +192,21 @@ app.post('/deleteNote', auth, (req, res) => {
 
     const noteId = req.body.noteId
     // const title = req.body.title
-    const sql =  'DELETE FROM notes WHERE id = ?'
+    const sql = 'DELETE FROM notes WHERE id = ?'
     connection.query(sql, [noteId], (err, results) => {
-        if (err){
+        if (err) {
             console.log("Query failed: ", err.message)
-            return res.status(500).send({message: "Query failed"})
+            return res.status(500).send({ message: "Query failed" })
         }
         else {
-            return res.status(201).send({message: "notes deleted successfully"})
+            return res.status(201).send({ message: "notes deleted successfully" })
         }
-        
+
     })
 })
 
 
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
